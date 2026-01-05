494df17017a5a4070da10081ccd01e17542bd3f3
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2026-01-05 05:39:02 +0100

    code clean-up

--- app/src/main/java/com/example/womencare/ui/home/MainDestinations.kt
+++ app/src/main/java/com/example/womencare/ui/home/MainDestinations.kt
@@ -11,49 +11,6 @@ import androidx.compose.material.icons.filled.Map
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.womencare.R
 
-/*
-
-sealed class MainBottomDestinations(
-    var route: String,
-    @StringRes var titleRes: Int,
-    var icon: ImageVector
-) {
-
-    object Home: MainBottomDestinations(
-        route = "overview",
-        titleRes = R.string.home,
-        icon = Icons.Filled.Home
-    )
-
-
-
-    object Library: MainBottomDestinations(
-        route = "library",
-        titleRes = R.string.library,
-        icon = Icons.Filled.Book
-    )
-    object Image: MainBottomDestinations(
-        route = "images",
-        titleRes = R.string.images,
-        icon = Icons.Filled.Architecture
-    )
-
-    object Map: MainBottomDestinations(
-        route = "map",
-        titleRes = R.string.maps,
-        icon = Icons.Filled.LocationOn
-    )
-}
-
-val BottomDestinationa = listOf(
-    MainBottomDestinations.Home,
-    MainBottomDestinations.Library,
-    MainBottomDestinations.Image,
-    MainBottomDestinations.Map
-)
-*/
-
-
 sealed class MainBottomDestinations(
     val route: String,
     @StringRes val titleEn: Int,
